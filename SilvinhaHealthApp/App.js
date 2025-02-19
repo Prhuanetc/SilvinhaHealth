11af2779c2926619e7882d7638b24a03e1c484b5
prhuanetec <email.rhuan.etec@gmail.com>
2025-02-19 13:59:59 -0300

    Criado o projeto

---
+++ SilvinhaHealthApp/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <view style={style.titleContainer}>
+        <text style={style.title}>Silvinha Health App</text>
+      </view>
+      <Text>Opaaaaa!</Text>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
